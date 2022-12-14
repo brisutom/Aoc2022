d2ae60d558fdace63ed17f2be327c70a95b5f18f
brisutom <brisuciak.t@gmail.com>
2022-12-14 17:07:37 +0100

    Day 14

---
+++ 14/part1.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from matplotlib import pyplot as plt
+
+
+DOWN = (0, 1)
+DOWN_LEFT = (-1, 1)
+DOWN_RIGHT = (1, 1)
+
+
+def add_tuples(a, b, n=1):
+    return (a[0] + n*b[0], a[1] + n*b[1])
+
+
+def sgn(x):
+    return tuple([int(y/abs(y)) if y != 0 else 0 for y in x])
+
+
+def add_line(d, start, end):
+    current = start
+    diff = add_tuples(end, start, -1)
+    step = sgn(diff)
+    while current != end:
+        d[current] = 1
+        current = add_tuples(current, step)
+    d[end] = 1
+
+
+def simulate_sand(d):
+    sand = (500, 0)
+    while True:
+        if d[add_tuples(sand, DOWN)] == 0:
+            sand = add_tuples(sand, DOWN)
+        elif d[add_tuples(sand, DOWN_LEFT)] == 0:
+            sand = add_tuples(sand, DOWN_LEFT)
+        elif d[add_tuples(sand, DOWN_RIGHT)] == 0:
+            sand = add_tuples(sand, DOWN_RIGHT)
+        else:
+            return sand
+        if sand[1] > max_depth:
+            return None
+
+
+lines = [x.strip("\n").split(" -> ") for x in open("input.txt")]
+lines = [[tuple(map(int, y.split(","))) for y in x] for x in lines]
+scan = defaultdict(int)
+max_depth = 0
+for line in lines:
+    for start, end in zip(line, line[1:]):
+        if max(start[1], end[1]) > max_depth:
+            max_depth = max(start[1], end[1])
+        add_line(scan, start, end)
+
+scan_rock = scan.copy()
+result = 0
+while True:
+    sand = simulate_sand(scan)
+    if sand is not None:
+        scan[sand] = 2
+        result += 1
+    else:
+        break
+
+print("Part 1: ", result)
+
+plt.gca().invert_yaxis()
+img = plt.scatter(*zip(*scan), color="yellow")
+plt.scatter(*zip(*scan_rock), color="black")
+plt.show()
